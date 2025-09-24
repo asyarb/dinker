a7d27115b87cf2ee4b6d858dde59e0dad12a70a7
Anthony Yarbrough <asyarb@hawaii.edu>
2025-09-24 13:31:02 -1000

    add more court data

--- scripts/create-court-data/index.ts
+++ scripts/create-court-data/index.ts
@@ -3,34 +3,38 @@ import pMap from "p-map"
 import * as prettier from "prettier"
 import raw from "./raw.json"
 
-// TODO: Find a way to generate `raw` via scraping
-
 type Court = (typeof raw)[number]
 
+const FIELDS = [
+	"places.id",
+	"places.location",
+	"places.formattedAddress",
+	"places.googleMapsUri",
+]
+
 const SEARCH_TEXT_ARGS = {
-	otherArgs: {
-		headers: { "X-Goog-FieldMask": "places.location" },
-	},
+	otherArgs: { headers: { "X-Goog-FieldMask": FIELDS.join(",") } },
 }
 
 const places = new PlacesClient({ apiKey: import.meta.env.GOOGLE_API_KEY })
 
 async function getCourtLocation(court: Court) {
-	console.info(`Finding coordinates for: ${court.name}...`)
+	console.info(`Finding info for: ${court.name}...`)
 
 	const [response] = await places.searchText(
 		{ textQuery: court.name, maxResultCount: 1 },
 		SEARCH_TEXT_ARGS,
 	)
 
-	const location = response.places?.at(0)?.location
-	if (location) {
-		console.info(
-			`Found. Latitude: ${location.latitude}, Longitude: ${location.longitude} `,
-		)
-	}
+	const place = response.places?.at(0)
 
-	return { ...court, location }
+	return {
+		...court,
+		id: place?.id,
+		location: place?.location,
+		address: place?.formattedAddress,
+		uri: place?.googleMapsUri,
+	}
 }
 
 const result = await pMap(raw, getCourtLocation, { concurrency: 5 })
